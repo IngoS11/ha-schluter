4964a7c56447f44733a126e8a280856f1e08893b
Ingo Sauerzapf <ingo.sauerzapf@gmail.com>
2023-08-12 21:13:17 +0000

    added simple form test

--- tests/test_config_flow.py
+++ tests/test_config_flow.py
@@ -1,123 +1,15 @@
-"""Test Home Assitant Schluter Custom Component config flow"""
-import pytest
+"""Test config flow."""
+from unittest.mock import patch
 
-from homeassistant import config_entries, data_entry_flow
-from homeassistant.core import HomeAssistant
-from homeassistant.const import CONF_API_TOKEN, CONF_SOURCE
+from homeassistant import config_entries, setup
 from custom_components.schluter.const import DOMAIN
 
-from . import (
-    CONF_DATA,
-    CONF_INPUT,
-    NAME,
-    create_entry,
-    mocked_schluter_info,
-    patch_schluter_login,
-)
 
-
-async def test_flow_user_step_no_input(hass: HomeAssistant) -> None:
-    """Test appropriate erro when no input is provided"""
-    _result = await hass.config_entries.flow.async_init(
-        DOMAIN,
-        context={"source": "user"},
+async def test_form(hass):
+    """Test we get the form."""
+    await setup.async_setup_component(hass, "persistent_notification", {})
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
     )
-    result = await hass.config_entries.flow.async_configure(
-        _result["flow_id"], user_input={}
-    )
-
-    assert {"base": "missing"} == result["errors"]
-
-
-# async def test_flow_user(hass: HomeAssistant) -> None:
-#     """Test user initialized flow."""
-#     result = await hass.config_entries.flow.async_init(
-#         DOMAIN,
-#         context={"source": context={"source": "user"}
-#     )
-#     with mocked_schluter_info(), patch_schluter_login():
-#         result = await hass.config_entries.flow.async_configure(
-#             result["flow_id"],
-#             user_input=CONF_INPUT,
-#         )
-#     assert result["type"] == data_entry_flow.FlowResultType.CREATE_ENTRY
-#     assert result["title"] == NAME
-#     assert result["data"] == CONF_DATA
-
-
-# @pytest.mark.asyncio
-# async def test_flow_user_already_configured(hass: HomeAssistant) -> None:
-#     """Test user initialized flow with duplicate server."""
-#     create_entry(hass)
-#     result = await hass.config_entries.flow.async_init(
-#         DOMAIN, context={"source": config_entries.SOURCE_USER}
-#     )
-#     with mocked_schluter_info(), patch_schluter_login():
-#         result = await hass.config_entries.flow.async_configure(
-#             result["flow_id"],
-#             user_input=CONF_INPUT,
-#         )
-#     assert result["type"] == data_entry_flow.FlowResultType.ABORT
-#     assert result["reason"] == "already_configured"
-
-
-# @pytest.mark.asyncio
-# async def test_flow_user_unkonwn_error(hass: HomeAssistant) -> None:
-#    """Test user initialized flow with reachable server."""
-#    with patch_schluter_login() as mock:
-#        mock.side_effect = Exception
-#        result = await hass.config_entries.async_init(
-#            DOMAIN,
-#            context={"source": config_entries.SOURCE_USER},
-#            data=CONF_DATA,
-#        )
-#    assert result["type"] == data_entry_flow.FlowResultType.FORM
-#    assert result["step_id"] == "user"
-#    assert result["errors"] == {"base": "unknown"}
-#
-#    with mocked_schluter_info(), patch_schluter_login():
-#        result = await hass.config_entries.flow.async_configure(
-#            result["flow_id"],
-#            user_input=CONF_INPUT,
-#        )
-#    assert result["type"] == data_entry_flow.FlowResultType.CREATE_ENTRY
-#    assert result["title"] == NAME
-#    assert result["data"] == CONF_DATA
-
-
-# @pytest.mark.asyncio
-# async def test_flow_reauth(hass: HomeAssistant) -> None:
-#    """Test a reauth flow."""
-#    entry = create_entry(hass)
-#    result = await hass.config_entries.flow.async_init(
-#        DOMAIN,
-#        context={
-#            CONF_SOURCE: config_entries.SOURCE_REAUTH,
-#            "entry_id": entry.entry_id,
-#            "unique_id": entry.unique_id,
-#        },
-#        data=entry.data,
-#    )
-#
-#    assert result["type"] == data_entry_flow.FlowResultType.FORM
-#    assert result["step_id"] == "reauth_confirm"
-#
-#    new_conf = {CONF_API_TOKEN: "1234567890123"}
-#    with patch_schluter_login() as mock:
-#        mock.side_effect = nextcord.LoginFailure
-#        result = await hass.config_entries.flow.async_configure(
-#            result["flow_id"],
-#            user_input=new_conf,
-#        )
-#    assert result["type"] == data_entry_flow.FlowResultType.FORM
-#    assert result["step_id"] == "reauth_confim"
-#    assert result["errors"] == {"base": "invalid_auth"}
-#
-#    with mocked_schluter_info(), patch_schluter_login():
-#        result = await hass.config_entries.flow.async_configure(
-#            result["flow_id"],
-#            user_input=new_conf,
-#        )
-#    assert result["type"] == data_entry_flow.FlowResultType.ABORT
-#    assert result["reason"] == "reauth_successful"
-#    assert entry.data == CONF_DATA | new_conf
+    assert result["type"] == "form"
+    assert result["errors"] == {}
